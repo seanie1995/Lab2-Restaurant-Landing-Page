8af275f78dc9980ab4c519da9105c6b944003e59
Sean Schelin <119659530+seanie1995@users.noreply.github.com>
2024-09-20 10:20:08 +0200

    Add project files.

---
+++ Lab1/Models/Dish.cs
@@ -0,0 +1,22 @@
+﻿using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+
+namespace Lab1.Models
+{
+    public class Dish
+    {    
+        public int Id { get; set; }
+        
+        [Required(ErrorMessage = "Title cannot be blank")]
+        [DisplayName("Film Title")]
+        public string Name { get; set; }
+
+        [Required(ErrorMessage = "Price cannot be blank")]
+        [DisplayName("Price")]
+        public int Price { get; set; }
+
+        [Required(ErrorMessage = "Availability cannot be blank")]
+        [DisplayName("Availability")]
+        public bool Availability { get; set; }
+    }
+}
