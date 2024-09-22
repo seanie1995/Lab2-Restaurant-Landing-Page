1e735628403780c59a421dbabf90fced59ad8bad
Sean Schelin <119659530+seanie1995@users.noreply.github.com>
2024-09-22 11:36:23 +0200

    Added EDIT functions successfuly

---
+++ Lab1/Views/Admin/Dishes.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace Lab1.Views.Admin
+{
+    public class DishesModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
